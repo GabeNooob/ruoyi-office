541752b5b62b14167aa65513fdaab87ebdb6f0df
289957361@qq.com <289957361@qq.com>
2025-07-13 10:42:01 +0800

    feat: 新增oa模块

---
+++ yudao-module-oa/yudao-module-oa-server/src/main/java/cn/iocoder/yudao/module/oa/OaServerApplication.java
@@ -0,0 +1,18 @@
+package cn.iocoder.yudao.module.oa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目的启动类
+ *
+ * @author 芋道源码
+ */
+@SpringBootApplication
+public class OaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OaServerApplication.class, args);
+    }
+
+}
